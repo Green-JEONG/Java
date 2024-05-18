6e74c5bffbfa18a4e1012e5cff9a46541d53b723
Green-JEONG <th2gr22n@gmail.com>
2024-05-18 19:59:11 +0900

    변수까지

---
+++ src/HelloJava.java
@@ -0,0 +1,6 @@
+public class HelloJava {
+
+    public static void main(String[] args) {        // main 메소드 : 프로그램의 시작점
+        System.out.println("hello java");           // 값을 콘솔에 출력하는 기능, 문자열 사용 시 따옴표!
+    }
+}
