a34930e42deaf8503556d610a9dffa28af73cedb
Green-JEONG <th2gr22n@gmail.com>
2024-05-19 17:10:57 +0900

    Ch2. 변수 완료

---
+++ src/variable/ex/VarEx1.java
@@ -0,0 +1,16 @@
+package variable.ex;
+
+public class VarEx1 {
+
+    public static void main(String[] args) {
+//        int a = 4 , b = 3;
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+        int num1 = 4;
+        int num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,15 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+//        long num1 = 10000000000L;
+//        System.out.println(num1);
+//        boolean num2 = true;
+//        System.out.println(num2);
+        long longVar = 10000000000L;
+        System.out.println(longVar);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+    }
+}
